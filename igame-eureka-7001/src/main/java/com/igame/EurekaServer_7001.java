f473ae955bca4f570bc6b9bc12fe3f9e98aadf50
ChanV <1277077741@qq.com>
2021-02-04 21:28:42 +0800

    2020-02-04上个版本忘记加@EnableEurekaServer……现在加上

--- igame-eureka-7001/src/main/java/com/igame/EurekaServer_7001.java
+++ igame-eureka-7001/src/main/java/com/igame/EurekaServer_7001.java
@@ -2,12 +2,14 @@ package com.igame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author ChanV
  * @create 2021-02-04-13:46
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServer_7001 {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer_7001.class, args);
