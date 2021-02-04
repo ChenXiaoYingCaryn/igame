a2514df333039e24c4cea31f8eb8a637e900ddce
ChanV <1277077741@qq.com>
2021-02-04 13:55:11 +0800

    搭建两个Eureka组成的Eureka集群

---
+++ igame-eureka-7001/src/main/java/com/igame/EurekaServer_7001.java
@@ -0,0 +1,15 @@
+package com.igame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ChanV
+ * @create 2021-02-04-13:46
+ */
+@SpringBootApplication
+public class EurekaServer_7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7001.class, args);
+    }
+}
---
+++ igame-eureka-7002/src/main/java/com/igame/EurekaServer_7002.java
@@ -0,0 +1,15 @@
+package com.igame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ChanV
+ * @create 2021-02-04-13:49
+ */
+@SpringBootApplication
+public class EurekaServer_7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7002.class, args);
+    }
+}
