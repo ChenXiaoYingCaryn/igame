c894257020c7b783c12551d445bd10b47c71dc8c
Chloe Chen <739130811@qq.com>
2021-02-05 13:27:26 +0800

    用户登录模块

--- igame-login-8001/src/main/java/com/igame/entity/User.java
+++ igame-login-8001/src/main/java/com/igame/entity/User.java
@@ -1,5 +1,7 @@
 package com.igame.entity;
 
+import java.util.Date;
+
 /**
  * @author xiaoying
  * @create 2021-02-04 下午 10:29
@@ -8,4 +10,73 @@ public class User {
     private String user_id = "";
     private String user_pwd = "";
     private String user_name = "";
+    private String user_sex = "";
+    private String user_email = "";
+    private String user_image = "";
+    private int is_deleted = 0;
+    private Date create_time;
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_pwd() {
+        return user_pwd;
+    }
+
+    public void setUser_pwd(String user_pwd) {
+        this.user_pwd = user_pwd;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getUser_sex() {
+        return user_sex;
+    }
+
+    public void setUser_sex(String user_sex) {
+        this.user_sex = user_sex;
+    }
+
+    public String getUser_email() {
+        return user_email;
+    }
+
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
+    }
+
+    public String getUser_image() {
+        return user_image;
+    }
+
+    public void setUser_image(String user_image) {
+        this.user_image = user_image;
+    }
+
+    public int getIs_deleted() {
+        return is_deleted;
+    }
+
+    public void setIs_deleted(int is_deleted) {
+        this.is_deleted = is_deleted;
+    }
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
 }
---
+++ igame-login-8001/src/main/java/com/igame/test/UserTest.java
@@ -0,0 +1,9 @@
+package com.igame.test;
+
+/**
+ * @author xiaoying
+ * @create 2021-02-05 上午 12:40
+ */
+public class UserTest {
+
+}
